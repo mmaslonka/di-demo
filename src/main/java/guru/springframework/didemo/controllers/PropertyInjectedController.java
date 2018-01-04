263973da635c779cf7c63457ba067562865fbc44
mmaslonka <mateusz.maslonka@gmail.com>
2018-01-04 15:38:05 +0100

    different ways for di

--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -1,4 +1,14 @@
 package guru.springframework.didemo.controllers;
 
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PropertyInjectedController {
+
+    public GreetingServiceImpl greetingService;
+
+    String sayHello(){
+        return greetingService.sayGreeting();
+    }
 }
