bd403281b9043e20f4ba7b4920bccd717c49e175
mmaslonka <mateusz.maslonka@gmail.com>
2018-01-05 14:27:37 +0100

    Added primary greeting service

--- src/main/java/guru/springframework/didemo/controllers/MyController.java
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -1,12 +1,19 @@
 package guru.springframework.didemo.controllers;
 
+import guru.springframework.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public void hello(){
-        System.out.println("Hello World!");
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String hello(){
+        return greetingService.sayGreeting();
     }
 
 }
